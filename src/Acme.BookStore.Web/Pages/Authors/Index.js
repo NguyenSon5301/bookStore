e4a195fd5e1f5f34fea924756f87895b45d544ea
son <nguyenson5301@gmail.com>
2022-07-19 14:52:57 +0700

    first commit

---
+++ src/Acme.BookStore.Web/Pages/Authors/Index.js
@@ -0,0 +1,104 @@
+﻿$(function () {
+    var l = abp.localization.getResource('BookStore');
+    var createModal = new abp.ModalManager(abp.appPath + 'Authors/CreateModal');
+    var editModal = new abp.ModalManager(abp.appPath + 'Authors/EditModal');
+
+    var dataTable = $('#AuthorsTable').DataTable(
+        abp.libs.datatables.normalizeConfiguration({
+            serverSide: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: true,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(acme.bookStore.authors.author.getList),
+            columnDefs: [
+                {
+                    title: l('Actions'),
+                    rowAction: {
+                        items:
+                            [
+                                {
+                                    text: l('Edit'),
+                                    visible:
+                                        abp.auth.isGranted('BookStore.Authors.Edit'),
+                                    icon: "fa-thin fa-gears",
+                                    action: function (data) {
+                                        editModal.open({ id: data.record.id });
+                                    },
+                                    
+
+                                },
+                                {
+                                    text: l('Delete'),
+                                    visible:
+                                        abp.auth.isGranted('BookStore.Authors.Delete'),
+                                    icon: "fas fa-trash-alt",
+                                    confirmMessage: function (data) {
+                                        return l(
+                                            'AuthorDeletionConfirmationMessage',
+                                            data.record.name
+                                        );
+                                    },
+                                    action: function (data) {
+                                        acme.bookStore.authors.author
+                                            .delete(data.record.id)
+                                            .then(function () {
+                                                abp.notify.info(
+                                                    l('SuccessfullyDeleted')
+                                                );
+                                                dataTable.ajax.reload();
+                                            });
+                                    }
+                                }
+                            ]
+                    }
+                },
+                {
+                    title: l('Name'),
+                    data: "name"
+                },
+                {
+                    title: l('BirthDate'),
+                    data: "birthDate",
+                    render: function (data) {
+                        return luxon
+                            .DateTime
+                            .fromISO(data, {
+                                locale: abp.localization.currentCulture.name
+                            }).toLocaleString();
+                    }
+                },
+                {
+                    title: l('ShortBio'),
+                    data: "shortBio",
+                }
+            ]
+        })
+    );
+
+    createModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+    editModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+    $('#NewAuthorButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    });
+    function getChartInstance() {
+        return $('#AuthorsTable').dxChart("instance"); 
+    }
+    $('#PrintAuthorButton').click(function (e) {
+        e.preventDefault();
+        //getTableInstance().print();
+        var HTML = $("html");
+        var cln = document.documentElement.innerHTML;
+        document.documentElement.innerHTML = cln;
+        HTML.html($('#AuthorsTable'));
+        window.print();
+        document.documentElement.innerHTML = cln;
+    });
+});
